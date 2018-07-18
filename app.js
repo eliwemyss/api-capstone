d5d307a8106c2690c52759163c18ea32d42e5cbf
Eli Wemyss <eliwemyss@gmail.com>
2018-07-18 13:52:05 -0700

    added new feature

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ $(".search").submit('',function(event){
                     let weatherData = displayWeather(data);
                     $('.results').html(weatherData);
                      getMusicData(data.weather[0].main);
-                     // catchError(data)
+                     pictureAPI(data.weather[0].main)
 
             });
    } else{$('.error').html(`<div class="error">*Please enter a valid zip code</div>`)}
@@ -29,13 +29,15 @@ function catchError(error) {
     }
  }
 
+
+
 function displayWeather(data) {
     return `
     <div class="weather-container">
-        <h2 class="weather-header">Current Weather for ${data.name}</h2>
+        <h2>Current Weather for ${data.name}</h2>
         <img class = "icon" src='http://openweathermap.org/img/w/${data.weather[0].icon}.png'>
-        <p class weather-info>${data.weather[0].main}</p>
-        <p class="weather-info">Current Temperature ${data.main.temp} &#176 F</p>
+        <p>${data.weather[0].main}</p>
+        <p>Current Temperature ${data.main.temp} &#176 F</p>
     </div>
     `
 }
@@ -79,4 +81,22 @@ function displayMusic(music){
         <div>
         `
 }
+
+// adding function to display background image with weather search query
+
+function pictureAPI(data){ 
+    var API_KEY = '9519885-4ad5c078aa2fe4ee145599bf0';
+    var PICTURE_URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(data);
+    $.getJSON(PICTURE_URL, function(data){
+    if (parseInt(data.totalHits) > 0)
+    $.each(data.hits, function(i, hit){ console.log(data.hits);
+        $('body').css('background-image', 'url(data.hits)');
+     });
+    else
+    console.log('No hits');
+    });
+}
+
+
+
 
